4b92b973f3f8996febc05f3fa63f8152588311f1
neevek <i@neevek.net>
2023-05-10 22:32:44 +0800

    refactor dashboard apis

--- src/dashboard/dashboard_server.rs
+++ src/dashboard/dashboard_server.rs
@@ -1,21 +1,37 @@
+use super::{JsonRequest, JsonResponse};
 use crate::Api;
 use anyhow::Result;
+use http::request::Parts;
 use http::response;
-use http_body::Full;
+use hyper::body;
 use hyper::{body::Body, body::Bytes, server::conn::Http, service::Service, Request, Response};
 use lazy_static::lazy_static;
 use log::{debug, error, info};
+use serde::de::DeserializeOwned;
+use serde::Serialize;
 use std::collections::HashMap;
 use std::future::Future;
 use std::sync::Arc;
 use std::{net::SocketAddr, pin::Pin};
 use tokio::net::TcpListener;
 
-static WEB_SOURCE_CODE: &[u8] = include_bytes!(concat!(env!("OUT_DIR"), "/rsproxy-web.blob"));
+static STATIC_RESOURCE: &[u8] = include_bytes!(concat!(env!("OUT_DIR"), "/rsproxy-web.blob"));
 const WEB_PREFIX: &'static str = "/web";
+const HEADER_KEY_CONTENT_TYPE: &'static str = "Content-Type";
+const CONTENT_TYPE_JSON: &'static str = "application/json";
+
+static HTTP_CODE_OK: u16 = 200;
+
+static SERVICE_ERR_CODE_INVALID_PAYLOAD: u16 = 100;
+static SERVICE_ERR_CODE_INVALID_RESPONSE: u16 = 101;
+static SERVICE_ERR_CODE_METHOD_NOT_SUPPORTED: u16 = 102;
+
+type RequestHandler = fn(api: Arc<dyn Api>, head: Parts, body: Bytes) -> Result<Body, JsonResponse>;
+type RequestHandlersMap = HashMap<&'static str, RequestHandler>;
+type StaticResourceMap = HashMap<&'static str, StaticResourceItem>;
 
 lazy_static! {
-    static ref WEB_SOURCE_CODE_MAPPING: HashMap<&'static str, WebSourceCodeItem> = {
+    static ref STATIC_RESOURCE_ITEM_MAPPING: StaticResourceMap = {
         const WEB_SOURCE_CODE_INDEX: &str =
             include_str!(concat!(env!("OUT_DIR"), "/rsproxy-web.idx"));
 
@@ -23,7 +39,7 @@ lazy_static! {
         for line in WEB_SOURCE_CODE_INDEX.lines() {
             let fields: Vec<&str> = line.split_whitespace().collect();
 
-            let item = WebSourceCodeItem {
+            let item = StaticResourceItem {
                 filename: fields[0],
                 offset: fields[1].parse().unwrap(),
                 len: fields[2].parse().unwrap(),
@@ -35,9 +51,14 @@ lazy_static! {
         }
         map
     };
+    static ref REQUEST_HANDLERS_MAP: RequestHandlersMap = {
+        let mut map = HashMap::<&'static str, RequestHandler>::new();
+        map.insert("/api/prefer_upstream", DashboardService::prefer_upstream);
+        map
+    };
 }
 
-struct WebSourceCodeItem {
+struct StaticResourceItem {
     filename: &'static str,
     offset: usize,
     len: usize,
@@ -57,9 +78,7 @@ impl DashboardServer {
         })?;
 
         let addr = listener.local_addr().unwrap();
-        info!("==========================================================");
         info!("dashboard server bound to: {}", addr);
-        info!("==========================================================");
         Ok(listener)
     }
 
@@ -86,79 +105,133 @@ impl DashboardServer {
     }
 }
 
-type RequestHandler = fn(&DashboardService, req: &Request<Body>) -> Response<Full<Bytes>>;
-type RequestHandlersMap = HashMap::<&'static str, RequestHandler>;
-
 struct DashboardService {
-    _api: Arc<dyn Api>,
-    req_handlers_map: RequestHandlersMap,
+    api: Arc<dyn Api>,
 }
 
 impl DashboardService {
     pub fn new(api: Arc<dyn Api>) -> Self {
-        let mut req_handlers_map = HashMap::<&'static str, RequestHandler>::new();
-        req_handlers_map.insert("/api/hello", DashboardService::hello);
-        DashboardService { _api: api, req_handlers_map }
+        DashboardService { api }
     }
 
-    fn handle_request(&self, req: &Request<Body>) -> Option<Response<Full<Bytes>>> {
+    async fn handle_request(api: Arc<dyn Api>, req: Request<Body>) -> Option<Response<Body>> {
         let path = req.uri().path();
-        match self.req_handlers_map.get(path) {
-            Some(handler) => Some(handler(&self, req)),
-            None => None
+        match REQUEST_HANDLERS_MAP.get(path) {
+            Some(handler) => {
+                let (parts, req_body) = req.into_parts();
+                let bytes = body::to_bytes(req_body).await.ok()?;
+                let resp_body = match handler(api.clone(), parts, bytes) {
+                    Ok(resp) => resp,
+                    Err(e) => Self::convert_resp_to_body(e).unwrap(),
+                };
+
+                Some(
+                    Response::builder()
+                        .status(HTTP_CODE_OK)
+                        .header(HEADER_KEY_CONTENT_TYPE, CONTENT_TYPE_JSON)
+                        .body(resp_body)
+                        .unwrap(),
+                )
+            }
+
+            None => None,
         }
     }
 
-    pub fn hello(&self, _req: &Request<Body>) -> Response<Full<Bytes>> {
-        let builder = Response::builder().status(200).header("Content-Type", "application/json");
-        builder.body(Full::new(Bytes::from(r#"{"key": "value"}"#))).unwrap()
+    fn not_supported() -> JsonResponse {
+        JsonResponse::fail(
+            SERVICE_ERR_CODE_METHOD_NOT_SUPPORTED,
+            "Method not supported".to_string(),
+        )
+    }
+
+    fn convert_req_to_json<T: DeserializeOwned>(
+        bytes: Bytes,
+    ) -> Result<JsonRequest<T>, JsonResponse> {
+        serde_json::from_slice::<JsonRequest<T>>(&bytes)
+            .map_err(|e| JsonResponse::fail(SERVICE_ERR_CODE_INVALID_PAYLOAD, e.to_string()))
+    }
+
+    fn convert_resp_to_body<T: Serialize>(resp: JsonResponse<T>) -> Result<Body, JsonResponse> {
+        let bytes = serde_json::to_vec(&resp)
+            .map_err(|e| JsonResponse::fail(SERVICE_ERR_CODE_INVALID_RESPONSE, e.to_string()))?;
+        Ok(Body::from(bytes))
+    }
+
+    fn prefer_upstream(api: Arc<dyn Api>, head: Parts, body: Bytes) -> Result<Body, JsonResponse> {
+        let body = match head.method {
+            http::Method::GET => Self::convert_resp_to_body(JsonResponse::<bool>::succeed(Some(
+                api.is_prefer_upstream(),
+            )))?,
+            http::Method::POST => {
+                let req = Self::convert_req_to_json::<bool>(body)?;
+                api.set_prefer_upstream(req.data.unwrap());
+                Self::convert_resp_to_body(<JsonResponse>::succeed(None))?
+            }
+            _ => return Err(Self::not_supported()),
+        };
+
+        Ok(body)
     }
 }
 
 impl Service<Request<Body>> for DashboardService {
-    type Response = Response<Full<Bytes>>;
+    type Response = Response<Body>;
     type Error = hyper::Error;
     type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
 
     fn call(&mut self, req: Request<Body>) -> Self::Future {
         fn static_html_resp(
             builder: response::Builder,
-            WebSourceCodeItem { filename: _, offset, len }: &WebSourceCodeItem,
-        ) -> Response<Full<Bytes>> {
-            let content = &WEB_SOURCE_CODE[*offset..(offset + len)];
-            builder.body(Full::new(Bytes::from(content))).unwrap()
+            StaticResourceItem {
+                filename: _,
+                offset,
+                len,
+            }: &StaticResourceItem,
+        ) -> Response<Body> {
+            let content = &STATIC_RESOURCE[*offset..(offset + len)];
+            builder.body(Body::from(content)).unwrap()
         }
 
-        let path = req.uri().path();
-        let path = if ["/", "/web/", WEB_PREFIX].iter().any(|&p| p == path) {
-            "/index.html"
-        } else {
-            if path.len() > 4 { &path[4..] } else { path }
-        };
+        let api = self.api.clone();
+        let res = async {
+            let path = req.uri().path();
+            let path = if ["/", "/web/", WEB_PREFIX].iter().any(|&p| p == path) {
+                "/index.html"
+            } else {
+                if path.len() > 4 {
+                    &path[4..]
+                } else {
+                    path
+                }
+            };
 
-        let res = match WEB_SOURCE_CODE_MAPPING.get(path) {
-            Some(item) => {
-                let mime = match mime_guess::from_path(item.filename).first() {
-                    Some(t) => t.to_string(),
-                    None => "application/octet-stream".to_string(),
-                };
+            Ok(match STATIC_RESOURCE_ITEM_MAPPING.get(path) {
+                Some(static_res) => {
+                    let mime = match mime_guess::from_path(static_res.filename).first() {
+                        Some(t) => t.to_string(),
+                        None => "application/octet-stream".to_string(),
+                    };
 
-                let builder = Response::builder().status(200).header("Content-Type", mime);
-                static_html_resp(builder, item)
-            }
-            None =>  {
-                match self.handle_request(&req) {
+                    let builder = Response::builder()
+                        .status(200)
+                        .header(HEADER_KEY_CONTENT_TYPE, mime);
+                    static_html_resp(builder, static_res)
+                }
+                None => match Self::handle_request(api, req).await {
                     Some(res) => res,
                     None => {
-                        let builder = Response::builder().status(404).header("Content-Type", "text/html");
-                        let item = WEB_SOURCE_CODE_MAPPING.get("/404.html").unwrap();
+                        let builder = Response::builder()
+                            .status(404)
+                            .header(HEADER_KEY_CONTENT_TYPE, "text/html");
+                        let item = STATIC_RESOURCE_ITEM_MAPPING.get("/404.html").unwrap();
                         static_html_resp(builder, item)
                     }
-                }
-            }
+                },
+            })
         };
 
-        Box::pin(async { Ok(res) })
+        Box::pin(res)
     }
 
     fn poll_ready(
--- src/dashboard/mod.rs
+++ src/dashboard/mod.rs
@@ -1,1 +1,34 @@
 pub(crate) mod dashboard_server;
+use serde::{Deserialize, Serialize};
+
+#[serde_with::skip_serializing_none]
+#[derive(Serialize, Deserialize)]
+pub struct JsonRequest<T> {
+    pub data: Option<T>,
+}
+
+#[serde_with::skip_serializing_none]
+#[derive(Serialize, Deserialize, Debug)]
+pub struct JsonResponse<T = ()> {
+    pub code: u16,
+    pub msg: String,
+    pub data: Option<T>,
+}
+
+impl<T> JsonResponse<T> {
+    pub fn succeed(data: Option<T>) -> Self {
+        JsonResponse::<T> {
+            code: 0,
+            msg: "".to_string(),
+            data,
+        }
+    }
+
+    pub fn fail(code: u16, msg: String) -> Self {
+        JsonResponse::<T> {
+            code,
+            msg: msg.to_string(),
+            data: None,
+        }
+    }
+}
