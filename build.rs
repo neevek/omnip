4c3c265f7cedaf3c1d23bf531d654c58743384f5
neevek <i@neevek.net>
2024-04-30 22:47:02 +0800

    fix comment

--- build.rs
+++ build.rs
@@ -8,7 +8,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let src_dir = "src/omnip-web/dist/";
     let out_dir = env::var("OUT_DIR")?;
     let archive_file_path = Path::new(&out_dir).join("omnip-web.blob");
-    // index into the omnip-web.blob file, in the format of [filepath offset length]
+    // index into the omnip-web.blob file, in the format of [filepath//offset//length//mime]
     let archive_index_file_path = Path::new(&out_dir).join("omnip-web.idx");
 
     monolithica::AssetArhiver::create_archive(
